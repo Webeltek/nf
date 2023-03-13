db39578fb7aaf8640ef6bcd80bacc45ea1a3fa8c
sta2 <sta2@st2.sta2.lan>
2023-03-13 12:45:15 +0100

    ldap conn  search filter test4

--- pythonworkshop/auth_bp/views.py
+++ pythonworkshop/auth_bp/views.py
@@ -73,8 +73,8 @@ def login_ldap(usr_email=None, usr_pass=None):
         if (conn.bound):
             print(f'ldap connection bound!')
         print(f'ldap conn info: {conn}')
-        print(f'ldap conn.schema{conn.schema}')
-        
+        print(f'ldap conn.schema{server.schema}')
+
         print(f'ldap entries[0]: {conn.entries[0]}')
         msg = print(conn.entries[0])    
     return jsonify({'ldap_conn':'response','msg':msg})
