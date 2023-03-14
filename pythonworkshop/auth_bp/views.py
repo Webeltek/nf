27d71d09b7e9da208a15ecdd5bd1605a3e549e29
sta2 <webeltek2017@gmail.com>
2023-03-14 13:58:17 +0100

    ldap conn plain  test3

--- pythonworkshop/auth_bp/views.py
+++ pythonworkshop/auth_bp/views.py
@@ -70,15 +70,15 @@ def login_ldap(usr_email=None, usr_pass=None):
         """ conn.search('uid=bookingapp,cn=sysaccounts,cn=etc,dc=int,dc=bitfrost,dc=no', 
                     '(&(uid=bookingapp)(memberOf=cn=room-booking-app-users,cn=groups,cn=accounts,dc=int,dc=bitfrost,dc=no))') """
         
-        conn.search('uid=bookingapp,cn=sysaccounts,cn=etc,dc=int,dc=bitfrost,dc=no', 
-                    '(&(uid=bookingapp))')
+        """ conn.search('uid=bookingapp,cn=sysaccounts,cn=etc,dc=int,dc=bitfrost,dc=no', 
+                    '(&(uid=bookingapp))') """
 
         #conn.start_tls()
 
         if (conn.bound):
             print(f'ldap connection bound!')
-            print(f'ldap conn.info{server.info}')
-            print(f'ldap entries: {conn.entries[0]}')
+            print(f'ldap conn.info{conn.result}')
+            #print(f'ldap entries: {conn.entries[0]}')
             print(f"ldap server.schema.object_classes['person']   :{server.schema.object_classes['person']}")
             #print(f'ldap search result entries[0]: {conn.entries[0]}')
             #get attributes of bookingapp entry and get email
