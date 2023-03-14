49de12739ebb0f785ce3b4370fbd0ed807413086
sta2 <webeltek2017@gmail.com>
2023-03-14 17:54:51 +0100

    ldap conn cursor test23

--- pythonworkshop/auth_bp/views.py
+++ pythonworkshop/auth_bp/views.py
@@ -87,7 +87,9 @@ def login_ldap(usr_email=None, usr_pass=None):
             r = Reader(conn, obj_person,'cn=accounts,dc=int,dc=bitfrost,dc=no')
             print(f'ldap Reader cursor: {r}')
             r.search()
-            print(f'ldap Reader search result{conn.response_to_json}')
+            resp_json = conn.response_to_json
+            print(f'ldap Reader search response uid {resp_json.dn.uid}'
+            print(f'ldap Reader search response: {resp_json}')
             #print(f"ldap server.schema.object_classes['person']   :{server.schema.object_classes['person']}")
             #print(f'ldap search result entries[0]: {conn.entries[0]}')
             #get attributes of bookingapp entry and get email
