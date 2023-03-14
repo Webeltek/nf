fcd95c0cf4ac20be74de377470c0cef211e1a6b3
sta2 <webeltek2017@gmail.com>
2023-03-14 14:07:30 +0100

    ldap conn plain  test4

--- pythonworkshop/auth_bp/views.py
+++ pythonworkshop/auth_bp/views.py
@@ -77,9 +77,10 @@ def login_ldap(usr_email=None, usr_pass=None):
 
         if (conn.bound):
             print(f'ldap connection bound!')
-            print(f'ldap conn.info{conn.result}')
+            print(f'ldap conn.extend.standard.who_am_i(): {conn.extend.standard.who_am_i()}')
+            print(f'ldap conn.result: {conn.result}')
             #print(f'ldap entries: {conn.entries[0]}')
-            print(f"ldap server.schema.object_classes['person']   :{server.schema.object_classes['person']}")
+            #print(f"ldap server.schema.object_classes['person']   :{server.schema.object_classes['person']}")
             #print(f'ldap search result entries[0]: {conn.entries[0]}')
             #get attributes of bookingapp entry and get email
             #conn.entries[0]
