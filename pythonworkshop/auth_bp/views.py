e9dc4ddb2b921464eb7aab2635fb3e59c008b273
sta2 <webeltek2017@gmail.com>
2023-03-15 14:46:23 +0100

    ldap conn cursor test65

--- pythonworkshop/auth_bp/views.py
+++ pythonworkshop/auth_bp/views.py
@@ -99,8 +99,8 @@ def login_ldap(usr_email=None, usr_pass=None):
 
             for entry in pyth_entries:
                 print(f'ldap cursor search entries.entry : {entry}')
-                #if entry.userPassword == ldap_pass: 
-                    #print(f'ldap Reader logged in user r.entries.entry.uid : {entry.uid} ')
+                if entry.userPassword == ldap_pass: 
+                    print(f'ldap Reader logged in user r.entries.entry.uid : {entry.uid} ')
 
             #print(f'ldap Reader search response: {resp_json}')
 
