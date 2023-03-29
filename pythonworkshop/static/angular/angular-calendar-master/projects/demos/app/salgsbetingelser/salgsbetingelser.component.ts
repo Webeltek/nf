14f1db0b56cb9179cef934331610f3ef244318eb
Webeltek <webeltek2017@gmail.com>
2023-03-29 00:16:20 +0000

    todo finalize login redirect2

---
+++ pythonworkshop/static/angular/angular-calendar-master/projects/demos/app/salgsbetingelser/salgsbetingelser.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mwl-salgsbetingelser',
+  templateUrl: './salgsbetingelser.component.html',
+  styleUrls: ['./salgsbetingelser.component.scss']
+})
+export class SalgsbetingelserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
