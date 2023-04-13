61a40dd19bb44503c561c06f459cb4939ae8a90f
webeltek2017@gmail.com <webeltek2017@gmail.com>
2023-04-13 14:59:45 +0200

    sqlalchemy bulk insert/delete2

--- pythonworkshop/static/angular/angular-calendar-master/projects/angular-calendar/src/modules/week/http-service.service.ts
+++ pythonworkshop/static/angular/angular-calendar-master/projects/angular-calendar/src/modules/week/http-service.service.ts
@@ -21,7 +21,7 @@ export class HttpEventService{
     private deleteRoomUrl = "/api/services/deleteroom";
     private updateRoomsUrl = "/api/services/updaterooms";
 
-    private baseurl = '';
+    private baseurl = 'https://api.webeltek.org';
 
     @Output() addedEvent: EventEmitter<any> = new EventEmitter();
 
